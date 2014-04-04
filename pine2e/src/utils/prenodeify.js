a4db6020991e0a8ab203b40139e016a0e9bfb2cc
Andrey Tarantsov <andrey@tarantsov.com>
2014-04-04 15:51:15 +0700

    Lift the function in prenodeify()

--- pine2e/src/utils/prenodeify.js
+++ pine2e/src/utils/prenodeify.js
@@ -1,15 +1,18 @@
 module.exports = prenodeify;
 
+var when = require('when');
 var nodeify = require('nodeify');
 
 function prenodeify(f) {
+  var lifted = when.lift(f);
+
   return function(...args) {
     if (args.length > 0 && typeof(args[args.length - 1]) === 'function') {
       var callback = args.pop();
       var promise = f.apply(this, args);
       nodeify(promise, callback);
     } else {
-      return f.apply(this, arguments);
+      return lifted.apply(this, arguments);
     }
   }
 }
